9b81f9ecbc6b75fe1ba9a3a8444e9236514c2613
amiralii <mr.roshanae@gmail.com>
2020-03-03 23:32:01 +0330

    fix domain service

---
+++ domain/service/promotion_test.go
@@ -0,0 +1,20 @@
+package service
+
+// import (
+// 	"reflect"
+//     "testing"
+// )
+
+
+// // func TestList(t *testing.T){
+
+// //     service := NewPromotionService(PromotionRepository)
+
+// //     var expected []*model.Promotion
+
+// //     actual, _ := service.List()
+
+// //     if reflect.TypeOf(actual) != expected{
+// //         t.Error("Wrong return type")
+// //     }
+// // }
